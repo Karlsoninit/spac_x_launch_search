4922c3ababc4c4bac5ca500ef54fddba0bbf598f
Karlsoninit <34502848+Karlsoninit@users.noreply.github.com>
2020-12-13 16:23:12 +0000

    Deploying to gh-pages from  @ 7e61b4de723330668e0dee9e67c9918e5c831e7e 🚀

---
+++ _next/static/XrbgM9_m2DFlYOTwNLg8z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F9ac62baa638228d52db3.css","static\u002Fchunks\u002Fpages\u002Findex-06aaabd80c0143d3c2f8.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-88b95d7d14c1825faba1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
